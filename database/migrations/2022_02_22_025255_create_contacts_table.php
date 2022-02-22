045dfb755ee04adb1305dd85c806b0c92f4c05a9
David Lopez <dlopez@mattmen.com>
2022-02-21 21:57:57 -0500

    Contacts Migration Column Creation. I created the columns for the table.

--- database/migrations/2022_02_22_025255_create_contacts_table.php
+++ database/migrations/2022_02_22_025255_create_contacts_table.php
@@ -15,6 +15,12 @@ return new class extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('country');
+            $table->text('message');
             $table->timestamps();
         });
     }
