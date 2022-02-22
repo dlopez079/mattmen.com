48fe5d5e3675f4a1eabaf47694abf3bcc1bd400c
David Lopez <dlopez@mattmen.com>
2022-02-22 12:05:04 -0500

    Hero Section: Size. I reduced the size for the large logo and adjusted the location.

--- resources/views/livewire/hero-section.blade.php
+++ resources/views/livewire/hero-section.blade.php
@@ -1,7 +1,7 @@
 <section id="hero">
     <div class="relative bg-black overflow-hidden">
       <div class="max-w-7xl mx-auto">
-        <div class="relative z-10 pb-8 bg-black sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+        <div class="relative z-10 pb-8 bg-black sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-25 xl:pb-28">
   
           <svg class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-black transform translate-x-1/2" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
             <polygon points="50,0 100,0 50,100 0,100" />
@@ -169,8 +169,8 @@
           </main>
         </div>
       </div>
-      <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 py-6">
-        <img class="animate-pulse h-56 w-full sm:h-72 md:h-96 lg:w-full lg:h-full" src="{{ asset('/img/SVG/mmlogo.svg') }}" alt="Mattmen Logo">
+      <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 py-16">
+        <img class="animate-pulse h-56 w-full sm:h-72 md:h-96" src="{{ asset('/img/SVG/mmlogo.svg') }}" alt="Mattmen Logo">
       </div>
     </div>
   </section>
