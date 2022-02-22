1b1ea23bfe4fa0c1960f7bfb42669154b886c9f3
David Lopez <dlopez@mattmen.com>
2022-02-22 12:08:34 -0500

    Hero Section: Text Modification. I changed the text from Front End and Back end Developer to Full Stack.

--- resources/views/livewire/hero-section.blade.php
+++ resources/views/livewire/hero-section.blade.php
@@ -151,7 +151,7 @@
                 <span class="block text-gray-600 xl:inline">dba Mattmen, Inc.</span>
               </h1>
               <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                Front-End and Back-End Developer with experience as a Graphic Designer, Project Manager, and IT Managed Service Consultant.
+                Full Stack Web/App Developer with experience as a Graphic Designer, Project Manager, and IT Managed Service Consultant.
               </p>
               <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div class="hidden rounded-md shadow">
