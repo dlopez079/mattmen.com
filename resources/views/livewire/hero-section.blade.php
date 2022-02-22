a3f99fcac73995aaa08d2e83e414a21cfb6c7b5e
David Lopez <dlopez@mattmen.com>
2022-02-22 18:14:03 -0500

    Hero Section: Title Modificaiton. I removed DBA from the title.  It will Just read David Lopez / Mattmen, Inc.

--- resources/views/livewire/hero-section.blade.php
+++ resources/views/livewire/hero-section.blade.php
@@ -157,7 +157,7 @@
             <div class="sm:text-center lg:text-left">
               <h1 class="text-4xl tracking-tight font-extrabold text-white sm:text-5xl md:text-6xl">
                 <span class="block xl:inline">David Lopez</span>
-                <span class="block text-gray-600 xl:inline">dba Mattmen, Inc.</span>
+                <span class="block text-gray-600 xl:inline">Mattmen, Inc.</span>
               </h1>
               <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
                 Full Stack Web/App Developer with experience as a Graphic Designer, Project Manager, and IT Managed Service Consultant.
